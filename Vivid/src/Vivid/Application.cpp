4a4c7d9a99f14f6517056ea37e284cfb6d6bbb68
vividskies <itsvividskies@gmail.com>
2025-08-27 17:29:23 +1000

    add window events

--- Vivid/src/Vivid/Application.cpp
+++ Vivid/src/Vivid/Application.cpp
@@ -1,20 +1,36 @@
 #include "vvpch.h"
 #include "Application.h"
 
-#include "Events/ApplicationEvent.h"
-#include "Log.h"
+//#include "Log.h"
 
 #include <GLFW/glfw3.h>
 
 namespace Vivid {
 
+#define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
+
 	Application::Application()
 	{
 		m_Window = std::unique_ptr<Window>(Window::Create());
+		m_Window->SetEventCallback(BIND_EVENT_FN(OnEvent));
 	}
 
 	Application::~Application()
 	{
+	}
+
+	void Application::OnEvent(Event& e)
+	{
+		EventDispatcher dispatcher(e);
+		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(OnWindowClose));
+
+		VV_CORE_INFO("{0}", e.ToString());
+	}
+
+	bool Application::OnWindowClose(WindowCloseEvent& e)
+	{
+		m_Running = false;
+		return true;
 	}
 
 	void Application::Run()
--- Vivid/src/Vivid/Application.h
+++ Vivid/src/Vivid/Application.h
@@ -2,6 +2,8 @@
 
 #include "Core.h"
 #include "Events/Event.h"
+#include "Events/ApplicationEvent.h"
+
 #include "Window.h"
 
 namespace Vivid {
@@ -13,7 +15,11 @@ namespace Vivid {
 		virtual ~Application();
 
 		void Run();
+
+		void OnEvent(Event& e);
 	private:
+		bool OnWindowClose(WindowCloseEvent& e);
+
 		std::unique_ptr<Window> m_Window;
 		bool m_Running = true;
 	};
